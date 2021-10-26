dcd52d75668c09d39c8855229859fe82ab81ecc1
johnprakash21 <92290958+johnprakash21@users.noreply.github.com>
2021-10-26 12:23:00 +0530

    Add files via upload

---
+++ john2.c
@@ -0,0 +1,12 @@
+// Addition of two numbers ( up to 3 decimal points)
+#include<stdio.h>
+int main()
+{
+	// variable declaration
+	int a, b, sum;
+	//initialization
+	a=76,b=21,sum=a+b;
+	// out put
+	printf(" The sum of two numbers a and b is :%d",sum);
+}
+
