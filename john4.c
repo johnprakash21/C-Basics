7eeb0abcc3c97bf9abea1cf9a272523b8361b4f4
johnprakash21 <92290958+johnprakash21@users.noreply.github.com>
2021-11-02 12:29:39 +0530

    Add files via upload

---
+++ john4.c
@@ -0,0 +1,13 @@
+// sum of n natural numbers 
+#include<stdio.h>
+int main()
+{
+// variable declaration	
+int n,s;
+// initialization
+n=10;
+//calculation
+s=(n*(n+1))/2;
+//out put
+printf(" The sum of first 5 natural numbers is %d",s);	
+}
